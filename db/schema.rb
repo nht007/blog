42a1336bc6acfd5811c5b99f9275d6bf6cab7ad9
Tien H. Nguyen <tien@ubuntu.(none)>
2011-06-02 09:52:46 -0500

    finished up rails tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110602142433) do
+ActiveRecord::Schema.define(:version => 20110602144231) do
 
   create_table "comments", :force => true do |t|
     t.string   "author"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(:version => 20110602142433) do
     t.string   "title"
     t.text     "content"
     t.datetime "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
