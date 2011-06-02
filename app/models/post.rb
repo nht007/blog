7158dfa3a77e5f8741c5e6f925c1c24f46632761
Tien H. Nguyen <tien@ubuntu.(none)>
2011-06-02 09:32:19 -0500

    scaffolding

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :author,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+ 
+  has_many :comments
 end
