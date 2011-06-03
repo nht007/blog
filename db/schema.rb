bc76743e9b772bf525167f74d077540f2037c5e1
Tien H. Nguyen <tien@ubuntu.(none)>
2011-06-03 15:37:33 -0500

    wiped old tags, installed acts_as_taggable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110602195945) do
+ActiveRecord::Schema.define(:version => 20110603203326) do
 
   create_table "comments", :force => true do |t|
     t.string   "author"
@@ -29,18 +29,21 @@ ActiveRecord::Schema.define(:version => 20110602195945) do
     t.datetime "updated_at"
   end
 
-  create_table "posts_tags", :id => false, :force => true do |t|
-    t.integer "tag_id"
-    t.integer "post_id"
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
+    t.datetime "created_at"
   end
 
-  add_index "posts_tags", ["post_id"], :name => "index_posts_tags_on_post_id"
-  add_index "posts_tags", ["tag_id", "post_id"], :name => "index_posts_tags_on_tag_id_and_post_id"
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
   create_table "tags", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name"
   end
 
 end
